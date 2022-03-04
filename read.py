9b31ef00e670710a6a136229cfd73deaf1f743c6
Sanctus <ruben.belchior@hotmail.com>
2022-03-04 16:41:34 +0000

    outliers treatment needed

---
+++ read.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+import PyPDF2
+from readability import Readability
+import numpy as np
+import re
+import os
+import glob
+
+
+#FULL_PATH = '/home/sanctus/projects/lama/nedbank_2018.pdf'
+
+
+
+def score(text: str) -> float:
+    '''
+    This will get me the score 
+    '''
+    r = Readability(text)
+    #flesch-kindcaid grade level
+    fk =  r.flesch_kincaid()
+    y = fk.score
+    x = fk.grade_level
+    print('This is the score: {}'.format(y))
+    print('This is the grade level: {}'.format(x))
+    return float(y),float(x)
+
+
+def get_text(path: str) -> None:
+    '''
+    This will transform the pdf into text capable of the Readability library to read it 
+    '''
+    scoring = []
+    grading = []
+
+    object = PyPDF2.PdfFileReader(str(path))
+    # gEt the # of pages
+    numpages = object.getNumPages()
+    analysed = 0
+    for i in range(0, numpages):
+        pageobj = object.getPage(i)
+        text = pageobj.extractText()
+        if len(text.split())>100:
+            analysed += 1
+            y,x = score(text)
+            scoring.append(y)
+            grading.append(x)
+    cover = (analysed/numpages)*100
+    s1, s2, s3 = concat(scoring)
+    g1, g2, g3 = concat(grading)
+    #print('SCORE\n mean: {}\n max: {}\n min: {}\n analysed: {}%'.format(s1,s2,s3,cover))
+    #print('GRADE\n mean: {}\n max: {}\n min: {}\n analysed: {}%'.format(g1,g2,g3,cover))
+    return s1,s2,s3,g1,g2,g3,cover
+
+def concat(values: list) -> float:
+    '''
+    This get the average, maximum and the minimum for the list recieved.
+    SHOULD FIX THE BUG BELOW AS MAKE FOR SURE THAT THE FUNCTION RECEIVES NUMBERS
+    '''
+    #values = [i for float(i) in values]
+    return (sum(values)/len(values)) , max(values),min(values)
+
+def baptise(path: str) -> str:
+    '''
+    This get us the company and the year
+    '''
+    base = path.split('/')
+    nameyear = base[-1]
+    y = nameyear.split('_')
+    name = y[0]
+    year = ((y[-1]).split('.'))[0]
+    print(name)
+    print(year)
+    return name, year
+
+def sample(path: str) -> None:
+    '''
+    For each report this function inserts the values on the file sample.txt
+    '''
+    smean, smax, smin, gmean, gmax, gmin, score = get_text(path)
+    name, year = baptise(path)
+    data = str(name)+','+str(year)+','+str(smean)+','+str(smax)+','+str(smin)+','+str(gmean)+','+str(gmax)+','+str(gmin)+','+str(score)+'\n'
+    print(data)
+    file_object = open('sample.txt', 'a')
+    file_object.write(data)
+    file_object.close()
+    #print(data)
+
+def engine():
+    mkdir = os.path.dirname(os.path.realpath(__file__))+"/*.pdf"
+    names = [os.path.basename(x) for x in glob.glob(str(mkdir))]
+    file_object = open('sample.txt', 'a')
+    file_object.write('firm,year,score_mean,score_max,score_min,grade_mean,grade_max,grade_mix,covered\n')
+    file_object.close()
+    for j in names:
+        sample(j)
+
+
+def main():
+    engine()
+
+if __name__=="__main__":
+    main()
