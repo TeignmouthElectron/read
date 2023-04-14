1b5eae08835622a96c452113876a01476d721197
Sanctus <ruben.belchior@hotmail.com>
2023-04-14 17:16:34 +0100

    get tone metric from LM dictionary

--- lama_paper/tone/sent_score_read.py
+++ lama_paper/tone/sent_score_read.py
@@ -1,43 +1,40 @@
 #!/usr/bin/env python3
-import PyPDF2
 import numpy as np
 import re
 import os
 import glob
-import pysentiment2 as ps
+import utils
+
 """
+Lastest update: 14/04/2023
 Readme:
-[] get the full text from pdf
-[] apply the sentiment score to the whole document
+[] get the full text from pdf with stopwords method to clean tables
+[] get the Lm word dictionary
+[returns]
+	-  firm
+	-  year
+	- total number of words
+	- number of Negative
+    - number of Positive
+    - number of Uncertainty
+    - number of Litigious
+    - number of Strong_Modal
+    - number of Weak_Modal
+    - number of Constraining
+	- % convered
 
 """
-
-
-def get_text(path: str) -> list:
-    '''
-    This will transform the pdf into text capable of the Readability library to read it
-    '''
-    object = PyPDF2.PdfReader(str(path))
-    numpages = len(object.pages)
-    analysed = 0
-    final = ''
-    for i in range(0, numpages):
-        pageobj = object.pages[i]
-        if pageobj.get_contents()==None:
-            continue
-        final += str(pageobj.extract_text())
-        analysed += 1
-    if numpages==0:
-        cover=0
-    else:
-        cover = (analysed/numpages)*100
-    return [final,cover] 
 
 def sent(text: str) -> dict:
-    lm = ps.LM()
-    tokens = lm.tokenize(text)
-    score = lm.get_score(tokens)
+    word_dict = utils.Lm_dict()
+    tokens = utils.tokenize(text)
+    score = {}
+    score['Total'] = len(tokens)
+    for j in word_dict.keys():
+        match = [i for i in tokens if i in word_dict[j]]
+        score[j] = len(match)  
     return score
+
 
 def baptise(path: str) -> str:
     '''
@@ -54,12 +51,12 @@ def sample(path: str) -> None:
     '''
     For each report this function inserts the values on the file sample.txt
     '''
-    y = get_text(path)
+    y = utils.get_text(path)
     cover = y[1]
     score = list((sent(y[0])).values())
     name, year = baptise(path)
-    print('[+] '+str(name)+' '+str(year)+' score: '+str(score)+'___covered:'+str(cover))
-    data = str(name)+','+str(year)+','+str(score[0])+','+str(score[1])+','+str(score[2])+','+str(score[3])+','+str(cover)+'\n'
+    print('[+] '+str(name)+' '+str(year)+'___covered:'+str(cover))
+    data = str(name)+','+str(year)+','+str(score[0])+','+str(score[1])+','+str(score[2])+','+str(score[3])+','+str(score[4])+','+str(score[5])+','+str(score[6])+','+str(score[7])+','+str(cover)+'\n'
     file_object = open('sample.txt', 'a')
     file_object.write(data)
     file_object.close()
@@ -69,7 +66,7 @@ def engine():
     mkdir = os.path.dirname(os.path.realpath(__file__))+"/*.pdf"
     names = [os.path.basename(x) for x in glob.glob(str(mkdir))]
     file_object = open('sample.txt', 'a')
-    file_object.write('firm,year,score_pos,score_neg,score_pol,score_sub,covered\n')
+    file_object.write('firm,year,total,neg,pos,uncertainty,litigious,strong,weak,constraining,covered\n')
     file_object.close()
     for j in names:
         sample(j)
